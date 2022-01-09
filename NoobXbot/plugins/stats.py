0443ea113f6202c27d7be6df1671d47db2b01cf5
Akku <96183105+userderdead@users.noreply.github.com>
2022-01-09 19:49:03 +0530

    Update and rename Stats.py to stats.py

--- NoobXbot/plugins/stats.py
+++ NoobXbot/plugins/stats.py
@@ -15,13 +15,13 @@ from pyrogram import __version__ as pyrover
 from pyrogram import filters
 from pyrogram.types import Message
 
-from Yukki import (BOT_ID, MUSIC_BOT_NAME, SUDOERS, app, boottime, pymongodb,
+from NoobXbot import (BOT_ID, SUDOERS, app, boottime, pymongodb,
                    userbot)
-from Yukki.Database import get_gbans_count, get_served_chats, get_sudoers
-from Yukki.Inline import (stats1, stats2, stats3, stats4, stats5, stats6,
+from NoobXbot.Database import get_gbans_count, get_served_chats, get_sudoers
+from NoobXbot.Inline import (stats1, stats2, stats3, stats4, stats5, stats6,
                           stats7)
-from Yukki.Plugins import ALL_MODULES
-from Yukki.Utilities.ping import get_readable_time
+from NoobXbot.plugins import ALL_MODULES
+from NoobXbot.plugins.ping import get_readable_time
 
 __MODULE__ = "Stats"
 __HELP__ = """
@@ -88,7 +88,7 @@ async def stats_markup(_, CallbackQuery):
         smex = f"""
 [•]<u>**System Stats**</u>
 
-**Arc Uptime:** {uptime}
+**NoobXRobot Uptime:** {uptime}
 **System Proc:** Online
 **Platform:** {sc}
 **Architecture:** {arch}
