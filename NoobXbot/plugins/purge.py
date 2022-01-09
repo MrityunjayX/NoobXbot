97efe41d19f77824002238280e06ecb923c2b62e
Akku <96183105+userderdead@users.noreply.github.com>
2022-01-09 12:54:21 +0530

    Update purge.py

--- NoobXbot/plugins/purge.py
+++ NoobXbot/plugins/purge.py
@@ -4,8 +4,8 @@ Syntax: .purge"""
 import asyncio
 from pyrogram import Client, filters
 
-from plugins.helper_functions.admin_check import admin_check
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from NoobXbot.helper_functions.admin_check import admin_check
+from NoobXbot.helper_functions.cust_p_filters import f_onw_fliter
 
 TG_MAX_SELECT_LEN = 100
 
