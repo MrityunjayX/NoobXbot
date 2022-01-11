b6dfdc26a0b8e527791207fc6b2fdd1b21823991
Akku <96183105+userderdead@users.noreply.github.com>
2022-01-11 23:00:21 +0530

    Update alive.py

--- NoobXbot/plugins/alive.py
+++ NoobXbot/plugins/alive.py
@@ -23,3 +23,24 @@ TIME_DURATION_UNITES = (
 async def _human_time_duration(seconds):
     if seconds == 0:
         return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITES:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0
+            parts.append('{} {}{})'
+                        .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+@Client.on_message(filters.command(["alive", f"alive@{BOT_USERNAME}"]))
+async def alive(client, message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_second()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await client.send_photo(message.chat.id,
+        photo=f"(https://telegra.ph/file/0b60fa847086549786219.jpg)
+        captin=f""" Master {message.from_user.mention()}, I'm NoobXRobot.**
+      **I'm working perfectly**
+      **Bot : 0.7 Jan Update**
+      **My Master : [{OWNER_USERNAME}](https://t.me/userderdead})**
+      **Service Time : `{uptime}`
+      **Thanks For Using Me Dude🎵**"",
