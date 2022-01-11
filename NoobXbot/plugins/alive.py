b50f3d219946b55c06dd4dae50be77cc2e4f7872
Akku <96183105+userderdead@users.noreply.github.com>
2022-01-11 23:08:27 +0530

    Update alive.py

--- NoobXbot/plugins/alive.py
+++ NoobXbot/plugins/alive.py
@@ -26,7 +26,7 @@ async def _human_time_duration(seconds):
     parts = []
     for unit, div in TIME_DURATION_UNITES:
         amount, seconds = divmod(int(seconds), div)
-        if amount > 0
+        if amount > s
             parts.append('{} {}{})'
                         .format(amount, unit, "" if amount == 1 else "s"))
     return ', '.join(parts)
